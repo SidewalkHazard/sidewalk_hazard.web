3afd3026c0fee1dd184d37df1de008dbe8bbe593
Will Drexler <wdrexler@mojolingo.com>
2012-12-01 16:44:32 -0500

    Tropo Improvements

--- tropo/sidewalkhazards.rb
+++ tropo/sidewalkhazards.rb
@@ -1,7 +1,8 @@
-if !!$message_type && $message_type == "SMS"
+if !!$message_type && $message_type.downcase == "sms"
   call "+1#{$numberToDial}", :network => "SMS"
 else
   call "+1#{$numberToDial}"
 end
+sleep 3
 say $msg
 log "called #{$numberToDial}"
