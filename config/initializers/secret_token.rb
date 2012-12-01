3010f12e5bc0d7c31b6e7362f1e171a7448a9bcb
Allan Davis <cajun.code@gmail.com>
2012-12-01 11:33:43 -0500

    Intial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidewalkHazardWeb::Application.config.secret_token = '0149571ef239e63b704ca3bdfce2d3bc754c04f9f37a5f2278bfaa5cd4d767f2823021fba3c9e4548cc8274ab7e030ac1f4ae3baff6201429d7f94b7baa5276c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SidewalkHazardWeb::Application.config.session_store :cookie_store, key: '_sidewalk_hazard.web_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SidewalkHazardWeb::Application.config.session_store :active_record_store
