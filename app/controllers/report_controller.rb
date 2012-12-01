2661ae3066fdcd14d20d4e04afd9b407f8984380
Will Drexler <wdrexler@mojolingo.com>
2012-12-01 15:22:43 -0500

    Update the report_controller

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,14 +3,17 @@ class ReportController < ApplicationController
   responds_to :json
   
   def index
+    @reports = Report.all
   end
 
   def show
+    @report = Report.find params[:id]
   end
 
   def create
+    phone_id = !!params[:phone_id] ? params[:phone_id] : UUIDTools.random_create.to_s
     @report = Report.create(lat: params[:lat], long: params[:long], complete: false, description: params[:description],
-                            why: params[:why], phone_id: params[:phone_id])
+                            why: params[:why], phone_id: phone_id)
     respond_with @report
   end
 end
